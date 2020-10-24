2494eeec751dd4f4b78237b51f190ef4d1f8c564
Nhan Phan <nphanacc@gmail.com>
2020-10-24 07:14:47 -0700

    Oct24_addFontAwesomeAndBootstrapSocial

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-courgette';
 import 'typeface-open-sans';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
